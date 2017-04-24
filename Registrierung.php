d852596b463a09f2ac73a83b4041acf06f8419c2
TyrionLRB <robinbatta.rb@gmail.com>
2017-04-24 12:06:49 +0200

    Update 12:06

--- Registrierung.php
+++ Registrierung.php
@@ -10,16 +10,13 @@ $pdo = new PDO('mysql:host=localhost;dbname=MusikDB', 'root', ''); //Verbindungs
 </head>
 <body>
 
-
 <?php
 $showFormular = true; //Variable ob das Registrierungsformular angezeigt werden soll
-
 if(isset($_GET['register'])) {   //Überprüfung ob GET-Parameter übergeben
  $error = false;
  $email = $_POST['Email'];
  $passwort = $_POST['Passwort'];
  $passwort2 = $_POST['Passwort2'];    //Formulardaten abgefragt
-
  if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {   //Überprüfung ob eingegebene E-Mail gültig
  echo 'Bitte eine gültige E-Mail-Adresse eingeben<br>';
  $error = true;
@@ -32,34 +29,28 @@ if(isset($_GET['register'])) {   //Überprüfung ob GET-Parameter übergeben
  echo 'Die Passwörter müssen übereinstimmen<br>';
  $error = true;
  }
-
  if(!$error) {      //Überprüfung, dass die E-Mail-Adresse noch nicht registriert wurde
  $statement = $pdo->prepare("SELECT * FROM nutzer WHERE Email = :Email");
  $result = $statement->execute(array('Email' => $email));
  $user = $statement->fetch();
-
  if($user !== false) {    //Ausgabe (Errormeldung) wenn E-Mail-Adresse schon vergeben ist.
  echo 'Diese E-Mail-Adresse ist bereits vergeben<br>';
  $error = true;
  }
  }
-
  //Keine Fehler, wir können den Nutzer registrieren
  if(!$error) {
  $passwort_hash = password_hash($passwort, PASSWORD_DEFAULT);     //Hashwert für Passwort wird berechnet
-
  $statement = $pdo->prepare("INSERT INTO nutzer (Email, Passwort) VALUES (:Email, :Passwort)");   //Eintrag eines neuen Nutzers
  $result = $statement->execute(array('Email' => $email, 'Passwort' => $passwort_hash));
-
  if($result) {
- echo 'Registrierung war erfolgreich. <a href="Login.php">Zum Login</a>';     //Erfolgsmeldung das Eintrag des Users erfolgt ist.
+ header('Location: Login.php');    //Erfolgsmeldung das Eintrag des Users erfolgt ist.
  $showFormular = false;       //Registrierungsformular wird nicht nochmal aufgerufen.
  } else {
  echo 'Fehler beim Speichern.<br>';     //Wenn Fehler beim Eintragen in Tabelle.
  }
  }
 }
-
 if($showFormular) {       //Formular in welches man die Nutzerdaten eintragen kann.
 ?>
 
@@ -79,7 +70,6 @@ Passwort wiederholen:<br>
 <?php
 }
 ?>
-
 
 </body>
 </html>
