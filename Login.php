61e9849480b727f4aeb46ec9bb9ae57136f903bf
TyrionLRB <robinbatta.rb@gmail.com>
2017-05-08 09:59:53 +0200

    Update 08.05

--- Login.php
+++ Login.php
@@ -1,49 +1,39 @@
 <?php
 session_start();
 $pdo = new PDO('mysql:host=localhost;dbname=MusikDB', 'root', '');
-
+$_SESSION['rechte'] = "";
 if(isset($_GET['login'])) {           //Abfrage ob Login Fornular abgesendet
  $email = $_POST['Email'];
  $passwort = $_POST['Passwort'];
-
  $statement = $pdo->prepare("SELECT * FROM nutzer WHERE Email = :Email");    //Datenbankabfrage ob E-Mail-Adresse Vorhanden
  $result = $statement->execute(array('Email' => $email));
  $user = $statement->fetch();
-
  //Überprüfung des Passworts
- if ($user !== false && password_verify($passwort, $user['Passwort'])) {   //Passwortüberprüfung.
-// $_SESSION['Nutzer_ID'] = $user['id'];
- header('Location: interpret.htm');
+ if ($user !== false && password_verify($passwort, $user['Passwort']) OR $passwort == $user['Passwort']) {   //Passwortüberprüfung.
+ $_SESSION['rechte'] = $user['Rechte'];
+ header('Location: interpret.php');
  } else {
  $errorMessage = "E-Mail oder Passwort war ungültig<br>";
  }
-
 }
+include('header.php');
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-  <link rel="stylesheet" href="menue.css" />
-  <title>Login</title>
-</head>
 <body>
-  <body style="background-image:url(sunset.jpg)"
-  >
-
 <?php
 if(isset($errorMessage)) {
  echo $errorMessage;
 }
 ?>
-
 <form action="?login=1" method="post">
 E-Mail:<br>
 <input type="Email" size="40" maxlength="250" name="Email"><br><br>
 
 Passwort:<br>
-<input type="Password" size="40"  maxlength="250" name="Passwort"><br>
+<input type="Password" size="40"  maxlength="250" name="Passwort"><br><br>
 
 <input type="submit" value="Abschicken">
+<input type="button" onclick="location.href='Registrierung.php'" value="Zur Registrierung">
 </form>
-</body>
-</html>
+<?php
+include('footer.php')
+ ?>
