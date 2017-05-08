d69850418f03e0507ea62e53b4e18fffd1b33775
TyrionLRB <robinbatta.rb@gmail.com>
2017-05-08 10:00:39 +0200

    Update logout.php

--- Logout.php
+++ Logout.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 session_destroy();
-
-echo "Logout erfolgreich";
+include('header.php');
+echo "<h1>Vielen Dank fuer Ihren Besuch!</h1>
+  <p>Ihr Logout war erfolgreich. Wir hoffen, dass Sie uns bald wieder besuchen!
+  </p>";
+include('footer.php')
 ?>
