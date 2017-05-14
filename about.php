3c067c415cfa2a1d714fdc1cd4eb666caea6d108
TyrionLRB <robinbatta.rb@gmail.com>
2017-05-14 17:29:09 +0200

    Final Update

---
+++ about.php
@@ -0,0 +1,16 @@
+<?php
+include('datenbankinitialisierung.php');
+include('header.php');
+include('menue.php');
+?>
+  <br>
+  <br>
+  <br>
+<h1>Vielen Dank fuer Ihren Besuch!</h1>
+  <p>Wir freuen uns, dass Sie sich entschieden haben unsere Seite zu besuchen.<br>
+    Diese Seite wurde von uns erstellt im Rahmen eines Leistungsnachweises fuer die Hochschule Weserbergland. <br>
+    Die Datenbank ist also nur rudimentaer mit Daten hinterlegt. Wir bitten dies zu entschuldigen.
+  </p>
+<?php
+include('footer.php');
+?>
