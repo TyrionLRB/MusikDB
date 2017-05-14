5c76dcf7b1ad4bc2f6208a765b078b451df97bf7
TyrionLRB <robinbatta.rb@gmail.com>
2017-05-14 17:29:43 +0200

    Final Update

--- datenbankinitialisierung.php
+++ datenbankinitialisierung.php
@@ -1,10 +1,9 @@
 <?php
-
 /*
 * In diesem PHP-Form wird die Verbindung zur Datenbank hergestellt.
 */
-
 session_start();
 $rechte = $_SESSION['rechte'];
 $pdo = new PDO('mysql:host=localhost;dbname=MusikDB', 'root', '');
 ?>
+
