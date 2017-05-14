94594902c80d09b7e97d0fff86964f7fb9c3a8d9
TyrionLRB <robinbatta.rb@gmail.com>
2017-05-14 17:22:33 +0200

    Final Update

--- Logout.php
+++ Logout.php
@@ -5,12 +5,13 @@
 * wird die Session des Users abgeschlossen und er bekommt eine Meldung, dass er
 * erfolgreich ausgeloggt wurde.
 */
-
 session_start();
 session_destroy();
 include('header.php');
 echo "<h1>Vielen Dank fuer Ihren Besuch!</h1>
   <p>Ihr Logout war erfolgreich. Wir hoffen, dass Sie uns bald wieder besuchen!
   </p>";
+
 include('footer.php')
 ?>
+
